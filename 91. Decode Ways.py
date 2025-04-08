0a6653dcdb11f79d177c2cc3a8f10ab630173f65
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2025-04-08 11:25:22 -0400

    Update 91. Decode Ways.py

--- 91. Decode Ways.py
+++ 91. Decode Ways.py
@@ -1,1 +1,7 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        if len(s) == 0:
+            return 0
 
+        if s[0] == '0': # any leading zero is invalid
+            return 0
