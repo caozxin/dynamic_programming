6bab66d94993584574dbd93745fb86b5a9ceddf8
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2025-04-11 05:39:45 -0400

    Update 322. Coin Change.py

--- 322. Coin Change.py
+++ 322. Coin Change.py
@@ -73,3 +73,47 @@ class Solution:
 
         result = dfs(0)
         return result if result != float('inf') else -1
+
+
+### better version still with backtracking and memoization:
+from math import inf
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+
+        def min_coins(coins, amount, sum, memo):
+            if sum == amount:
+                return 0
+
+            if sum > amount:
+                return inf
+
+            if (memo[sum] != -1):
+                return memo[sum]
+
+            ans = inf
+            for coin in coins:
+                result = min_coins(coins, amount, sum + coin, memo)
+                if result == inf:
+                    continue
+                ans = min(ans, result + 1)
+
+            memo[sum] = ans
+            return ans
+
+        # def coin_change(coins: List[int], amount: int) -> int:
+        memo = [-1] * (amount + 1)
+        result = min_coins(coins, amount, 0, memo)
+        return result if result != inf else -1
+
+### best version with Bottom-Up DP:
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [float('inf')] * (amount + 1)
+        dp[0] = 0  # base case
+
+        for coin in coins:
+            for x in range(coin, amount + 1):
+                dp[x] = min(dp[x], dp[x - coin] + 1)
+
+        return dp[amount] if dp[amount] != float('inf') else -1
+
